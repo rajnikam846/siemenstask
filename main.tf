7bf09f4a50c1f15e6cf6771df25420aaa77ce6e2
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 11:16:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,7 @@
 resource "aws_vpc" "myVPC" {
-  data_id = data.aws_vpc.vpc.id
+  vpc_id = data.aws_vpc.vpc.id
   tags = {
     "Name" = "var.vpc_name"
   }
 }
 
-resource "aws_nat_gateway" "example" {
- data = data.aws_nat_gateway.nat.id
-
-  tags = {
-    Name = "gw NAT"
-  }
